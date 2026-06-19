0bff72c7b8a5cf2c0cae9bf2ae22f76d6b303b87
Fernando Bueno <110437830+luisfernandobueno@users.noreply.github.com>
2026-06-19 02:26:52 +0000

    ICON UPDATED

--- src/search.js
+++ src/search.js
@@ -1,14 +1,13 @@
 // https://luisfernandobueno.github.io/json/jipapp.json
 const url_interview_data = "https://getpantry.cloud/apiv1/pantry/3892fc79-3651-48dd-aa62-75da3e708be7/basket/my-new-basket-name";
-
-let searchBar = document.getElementById("searchBar");
-let displayAllQuestions = document.getElementById("displayAllQuestions");
+const searchBar = document.getElementById("searchBar");
+const displayAllQuestions = document.getElementById("displayAllQuestions");
 
 
 function displayAll(data) {
 
     data.forEach(e => {
-        displayAllQuestions.innerHTML += `<p onclick=()>${e.question}</p>`
+        displayAllQuestions.innerHTML += `<li><a href='index.html'">${e.question}</a></li>`
         console.log(e.question)
     })
 }
@@ -33,8 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
             originalData = json;
             data = json.lines;
             console.log(data)
-
-            
 
             displayAll(data);
 
